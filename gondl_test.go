16ebbf25ad625ca7f87b22a91e31dd280327afb8
Danny Ben Shitrit <dannyb@gmail.com>
2014-10-16 21:02:06 +0300

    Updated tests for travis compatibility

--- gondl_test.go
+++ gondl_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func Example_GetSimple() {
-	run([]string{"get", "WIKI/CSCO", "-n4", "-t2014-01-01", "-T2014-01-04"})
+	run([]string{"get", "WIKI/CSCO", "-n4", "-t2014-01-01", "-T2014-01-04", "-c0"})
 	// Output:
 	// Date,Close
 	// 2014-01-03,21.98
@@ -16,14 +16,14 @@ func Example_GetSimple() {
 }
 
 func Example_GetMulti() {
-	run([]string{"get", "WIKI/AAPL.4", "WIKI/MSFT.4", "-t2014-01-01", "-T2014-01-04", "-x"})
+	run([]string{"get", "WIKI/AAPL.4", "WIKI/MSFT.4", "-t2014-01-01", "-T2014-01-04", "-x", "-c0"})
 	// Output:
 	// 2014-01-03,540.98,36.91
 	// 2014-01-02,553.13,37.16
 }
 
 func Example_GetCollapse() {
-	run([]string{"get", "WIKI/AAPL", "-t2014-01-01", "-T2014-02-01", "-x", "-n4", "--collapse=weekly"})
+	run([]string{"get", "WIKI/AAPL", "-t2014-01-01", "-T2014-02-01", "-x", "-n4", "--collapse=weekly", "-c0"})
 	// Output:
 	// 2014-02-02,500.6
 	// 2014-01-26,546.07
@@ -33,7 +33,7 @@ func Example_GetCollapse() {
 }
 
 func Example_GetTransformation() {
-	run([]string{"get", "WIKI/MSFT", "-t2014-01-01", "-T2014-01-10", "-sasc", "-n4", "--transformation=normalize"})
+	run([]string{"get", "WIKI/MSFT", "-t2014-01-01", "-T2014-01-10", "-sasc", "-n4", "--transformation=normalize", "-c0"})
 	// Output:
 	// Date,Close
 	// 2014-01-02,100.0
@@ -46,7 +46,7 @@ func Example_GetTransformation() {
 }
 
 func Example_GetFile() {
-	run([]string{"get", "WIKI/AAPL", "-n4", "-t2014-01-01", "-T2014-01-04", "-otmp", "-sasc"})
+	run([]string{"get", "WIKI/AAPL", "-n4", "-t2014-01-01", "-T2014-01-04", "-otmp", "-sasc", "-c0"})
 	data, _ := ioutil.ReadFile("tmp")
 	os.Remove("tmp")
 	fmt.Println(string(data))
@@ -57,7 +57,7 @@ func Example_GetFile() {
 }
 
 func Example_List() {
-	run([]string{"list", "WIKI", "-P2", "-fjson", "-otmp"})
+	run([]string{"list", "WIKI", "-P2", "-fjson", "-otmp", "-c0"})
 	data, _ := ioutil.ReadFile("tmp")
 	os.Remove("tmp")
 	var r map[string]interface{}
@@ -73,7 +73,7 @@ func Example_List() {
 }
 
 func Example_Search() {
-	run([]string{"search", "nasdaq composite", "-P3", "-fjson", "-otmp"})
+	run([]string{"search", "nasdaq composite", "-P3", "-fjson", "-otmp", "-c0"})
 	data, _ := ioutil.ReadFile("tmp")
 	os.Remove("tmp")
 	var r map[string]interface{}
